74d2c7902141f093cfe88e6d4b474ce5a7537c15
Kunle Oni <onikunle03@gmail.com>
2023-05-04 22:22:25 +0100

    solution

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Minimum operations task
+"""
+
+def minOperations(n):
+    """
+    :param n:
+    :return:
+    """
+    if n <= 1:
+        return 0
+    for i in range(2, n+1):
+        if n % i == 0:
+            return minOperations(int(n/i)) + i
