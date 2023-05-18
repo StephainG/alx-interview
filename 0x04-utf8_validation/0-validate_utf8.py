ad28b6c6b374a790c367b83fc8b5cc6cf92db32b
Kunle Oni <onikunle03@gmail.com>
2023-05-18 17:45:40 +0100

    utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+def validUTF8(data):
+    """To determine if a given data set
+    represents a valid utf-8 encoding
+    """
+    nb = 0
+
+    m_1 = 1 << 7
+    m_2 = 1 << 6
+
+    for i in data:
+
+        mb = 1 << 7
+
+        if nb == 0:
+
+            while mb & i:
+                nb += 1
+                mb = mb >> 1
+
+            if nb == 0:
+                continue
+
+            if nb == 1 or nb > 4:
+                return False
+
+        else:
+            if not (i & m_1 and not (i & m_2)):
+                return False
+
+        nb -= 1
+
+    if nb == 0:
+        return True
+
+    return False
